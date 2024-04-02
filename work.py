dff42a0c890b7aa59cfdd354297b950f5671cb58
rajvijay0902 <71257832+rajvijay0902@users.noreply.github.com>
2024-04-02 11:54:12 +0530

    Update work.py

--- work.py
+++ work.py
@@ -1,17 +1,13 @@
 import re
 import pandas as pd
 import os
-# Example usage
-import os
 
-# Get the absolute path of the current working directory
+
 current_dir = os.getcwd()
 
-# Extract the folder name from the absolute path
 folder_name = os.path.basename(current_dir)
 
 print("Current folder name:", folder_name)
-# Function to extract timestamp from folder name
 def extract_timestamp(folder_name):
     match = re.search(r'(\d{14})', folder_name)
     if match:
@@ -19,13 +15,9 @@ def extract_timestamp(folder_name):
     else:
         return None
 
-# Function to process CSV file
 def process_csv(folder_name):
-    # Extract timestamp from folder name
     timestamp = extract_timestamp(folder_name)
     df = pd.read_csv("sample2.csv")
-
-# Add a new column with the current timestamp
     df['Timestamp'] = timestamp
     print(df)
     df.to_csv('sample2.csv')
